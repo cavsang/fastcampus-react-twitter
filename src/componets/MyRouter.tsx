045f4d1926eccabfd08f69db3ccd78923e11bb9d
cavsang <cavsang@naver.com>
2024-04-28 17:47:25 +0900

    sec

---
+++ src/componets/MyRouter.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Route, Routes, Navigate } from 'react-router-dom';
+import HomePage from 'pages/HomePage';
+import PostsPage from 'pages/PostsPage';
+import PostNewPage from 'pages/PostNewPage';
+import PostEditPage from 'pages/PostEditPage';
+import PostDetailPage from "pages/PostDetailPage"
+import ProfilePage from 'pages/ProfilePage';
+import ProfileEditPage from 'pages/ProfileEditPage';
+import NotificationPage from 'pages/NotificationPage';
+import SearchPage from 'pages/SearchPage';
+import SignInPage from 'pages/SignInPage';
+import SignUpPage from 'pages/SignUpPage';
+
+export default function MyRouter(){
+    return (
+        <Routes>
+            <Route path="/" element={<HomePage />}></Route>
+            <Route path="/posts" element={<PostsPage />}></Route>
+            <Route path="/posts/:id" element={<PostDetailPage />}></Route>
+            <Route path="/posts/new" element={<PostNewPage />}></Route>
+            <Route path="/posts/edit/:id" element={<PostEditPage />}></Route>
+            <Route path="/profile" element={<ProfilePage />}></Route>
+            <Route path="/profile/edit" element={<ProfileEditPage />}></Route>
+            <Route path="/notification" element={<NotificationPage />}></Route>
+            <Route path="/search" element={<SearchPage />}></Route>
+            <Route path="/signin" element={<SignInPage />}></Route>
+            <Route path="/signup" element={<SignUpPage />}></Route>
+
+            <Route path="*" element={<Navigate replace to="/" />} />
+        </Routes>
+    )
+}
---
+++ src/pages/MyRouterPage.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Route, Routes, Navigate } from 'react-router-dom';
+
+export default function MyRouter(){
+    return (
+        <Routes>
+            <Route path="/" element={<h1>Home page</h1>}></Route>
+            <Route path="/posts" element={<h1>Posts page</h1>}></Route>
+            <Route path="/posts/:id" element={<h1>Post deatil page</h1>}></Route>
+            <Route path="/posts/new" element={<h1>Post new page</h1>}></Route>
+            <Route path="/posts/edit/:id" element={<h1>Post edit page</h1>}></Route>
+            <Route path="/profile" element={<h1>Profile page</h1>}></Route>
+            <Route path="/profile/edit" element={<h1>Profile Edit page</h1>}></Route>
+            <Route path="/notification" element={<h1>Notification page</h1>}></Route>
+            <Route path="/search" element={<h1>search page</h1>}></Route>
+            <Route path="/login" element={<h1>Login page</h1>}></Route>
+            <Route path="/signup" element={<h1>signup page</h1>}></Route>
+
+            <Route path="*" element={<Navigate replace to="/" />} />
+        </Routes>
+    )
+}
