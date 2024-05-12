98c49d47ab004783b9fe4f213313817457da47c3
cavsang <cavsang@naver.com>
2024-05-12 13:57:26 +0900

    0512home

--- src/util/Firebase.tsx
+++ src/util/Firebase.tsx
@@ -1,5 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp, FirebaseApp, getApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -22,6 +23,8 @@ try {
 } catch (error) {
     app = initializeApp(firebaseConfig, "app");
 }
+
+export const db = getFirestore(app);
 
 //const firebase = initializeApp(firebaseConfig);
 //export default firebase;
