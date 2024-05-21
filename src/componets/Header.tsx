631f34fe1c2aeec69d1b44f87a8f9887123c3ae1
cavsang <cavsang@naver.com>
2024-05-21 22:00:45 +0900

    0521home

--- src/componets/Header.tsx
+++ src/componets/Header.tsx
@@ -3,16 +3,19 @@ import React from 'react';
 type headerType = {
     title1: string,
     title2: string,
-    title3: string
+    title3: string,
+    select? : string,
+    setSelect? : any
 }
 
-export default function Header({title1, title2, title3}:headerType){
+export default function Header({title1, title2, title3, select, setSelect }:headerType){
+
     return (
         <div className="home__top">
             <div className="home__title">{title1}</div>
             <div className="home__tabs">
-    <div className="home__tab home__tab--active">{title2}</div>
-    <div className="home__tab">{title3}</div>
+    <div className={`home__tab ${select === "For You" ? "home__tab--active" : ""}`} onClick={() => {setSelect("For You")}}>{title2}</div>
+    <div className={`home__tab ${select === "Follow" ? "home__tab--active" : ""}`} onClick={() => {setSelect("Follow")}}>{title3}</div>
             </div>
         </div>
     )
--- src/componets/Home.tsx
+++ src/componets/Home.tsx
@@ -2,7 +2,7 @@ import React, { useState, useContext, useEffect } from 'react';
 import PostForm from './postForm';
 import PostBox from './PostBox';
 import AuthContext from 'util/AuthContext';
-import { collection, query, orderBy, onSnapshot } from 'firebase/firestore';
+import { collection, query, orderBy, onSnapshot, doc, getDoc, where } from 'firebase/firestore';
 import { db } from 'util/Firebase';
 import Header from './Header';
 import { PostProps } from 'util/MyTypes';
@@ -10,31 +10,54 @@ import { PostProps } from 'util/MyTypes';
 export default function Home() {
 
     const[posts, setPosts] = useState<PostProps[]>([]);
+    const [select, setSelect] = useState<string>("For You");
+    //const [fusers, setFusers] = useState<string[]>([]);
     const {user} = useContext(AuthContext);
+
+    const getFollwingDocs = async () => {
+        const docRef = doc(db, 'following',user?.uid as string);
+        const docs = await getDoc(docRef);
+        return docs?.data();
+    }
 
     useEffect(() => {
         if(user){
-            let postRef = collection(db, "posts");
-            let postQuery = query(postRef, orderBy("createAt","desc"));
+            if(select === "For You"){
+                let postRef = collection(db, "posts");
+                let postQuery = query(postRef, orderBy("createAt","desc"));
 
-            onSnapshot(postQuery, (snapshot) => {
-                let dataObj = snapshot.docs.map((doc) => ({
-                    ...doc.data(),
-                    id: doc?.id
-                }));
+                onSnapshot(postQuery, (snapshot) => {
+                    let dataObj = snapshot.docs.map((doc) => ({
+                        ...doc.data(),
+                        id: doc?.id
+                    }));
                 setPosts(dataObj as PostProps[]);
-            });
-        }
+                });
 
-    },[user]);
+            }else{
+                getFollwingDocs().then(doc => {
+                    let postRef = collection(db, "posts");
+                    let postQuery = query(postRef,where("uid","in",doc?.users), orderBy("createAt","desc"));
+
+                    onSnapshot(postQuery, (snapshot) => {
+                        let dataObj = snapshot.docs.map((doc) => ({
+                            ...doc.data(),
+                            id: doc?.id
+                        }));
+                    setPosts(dataObj as PostProps[]);
+                    });
+                });
+            }
+        }
+    },[user, select]);
     
 
     return (
         <div className="home">
             
-            <Header title1="Home" title2="For You" title3="Follow" />
+            <Header title1="Home" title2="For You" title3="Follow" select={select} setSelect={setSelect}/>
             
-            <PostForm />
+            {select && select === "For You" && (<PostForm />)}
 
             {/* tweet post */}
             <div className="post">
